1d066744fc9a1fa92279458847e54ef28f845983
Marcelozoo <marcelo.pinheiro01@hotmail.com>
2023-05-03 13:51:24 -0300

    configs: Setting project configs

---
+++ src/main.py
@@ -0,0 +1,11 @@
+def soma(numero_1, numero_2):
+    """_summary_
+
+    Args:
+        numero_1 (_type_): _description_
+        numero_2 (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return numero_1 + numero_2
---
+++ src/main_test.py
@@ -0,0 +1,7 @@
+from main import soma
+
+
+def test_soma():
+    """"""
+    result = soma(2, 4)
+    assert result == 6
